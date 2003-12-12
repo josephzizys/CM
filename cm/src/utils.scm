95be3bb7946ccbf579591b5e15c809b679b10a32
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2003-12-12 22:29:48 +0000

    added cm-icon
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@71 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/utils.scm
+++ cm/src/utils.scm
@@ -360,6 +360,29 @@
   (format #f "Common Music ~a" (cm-version-number)))
 
 ;;;
+;;; the ultimate in algorithmic iconic artware
+;;;
+
+(define (cm-logo )
+  (do ((e "~%")
+       (v (make-string 15))
+       (y 0 (+ y 1)))
+      ((= y 7) (values))
+    (format #t
+            (do ((x 0 (+ x 1)))
+                ((= x 15)
+                 (if (= y 3)
+                   (string-append v " " (cm-version) e)
+                   (string-append v e)))
+              (string-set! v x
+                           (if (<= 2 (- x y) 4) #\\
+                               (if (= (- x (- 4 (modulo (+ 13 y) 15))) 1)
+                                 #\/
+                                 (if (<= 1 y 5) #\-
+                                     (if (= (* (- x 6) (- y 3)) 15) #\/
+                                         #\space)))))))))
+
+;;;
 ;;; string hacks
 ;;;
 
