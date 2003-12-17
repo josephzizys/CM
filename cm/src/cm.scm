6b143117eb0378a7a4cdc4a0b41d7868b9eb4e49
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2003-12-17 17:54:39 +0000

    fix cm loading
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@76 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/cm.scm
+++ cm/src/cm.scm
@@ -9,11 +9,11 @@
 (if (< (string->number (minor-version) ) 5)
   (error "CM runs in guile-1.5.0 or higher."))
 
-;;; user defined cm-directory or default to current 
+;;; user defined *cm-root* or default to current 
 
-(if (not (module-bound? (current-module) 'cm-directory))
+(if (not (module-bound? (current-module) '*cm-root*))
   (begin
-   (define cm-directory #f)
+   (define *cm-root* #f)
 
    ;; courtesy of bil, removed %load-hook
    (let ((curfile (port-filename (current-load-port)))
@@ -24,16 +24,15 @@
        (if (char=? (string-ref curfile i) #\/)
          (begin (set! parent-slash last-slash)
                 (set! last-slash i))))
-     (set! cm-directory (substring curfile 0 (1+ parent-slash)))
+     (set! *cm-root* (substring curfile 0 (1+ parent-slash)))
      (let ((new-path (substring curfile 0 last-slash)))
        (if (not (member new-path %load-path))
          (set! %load-path (cons new-path %load-path)))))))
 
 ;;; user specified cm-bin-directory or default
-
-(if (not (module-bound? (current-module) 'cm-bin-directory))
-  (define cm-bin-directory
-    (string-append cm-directory "bin/")))
+;(if (not (module-bound? (current-module) 'cm-bin-directory))
+;  (define cm-bin-directory
+;    (string-append cm-directory "bin/")))
 
 (do ((files '("guile"
 	      "goops"
@@ -61,20 +60,25 @@
             (cdr files)))
     ((null? files) #f)
   (let* ((f (string-append (car files) ".scm")))
-    (display (string-append "; Loading: " cm-directory
+    (display (string-append "; Loading: " *cm-root*
 			    "src/" f))
     (newline)
     (load (string-append (car files) ".scm"))))
 
-;;; install default cminit.lisp file in bin directory
+;;; maybe install default cminit.lisp file in bin directory
 
-(let ((init (string-append cm-bin-directory "cminit.lisp"))
-      (orig (string-append cm-directory "etc/cminit.lisp")))
+(let ((init (string-append *cm-root* "lib/cminit.lisp"))
+      (orig (string-append *cm-root* "etc/cminit.lisp")))
  (if (not (file-exists? init))
    (if (file-exists? orig)
      (copy-file orig init)
      (format #t "; Help! Can't copy ~s -> ~s."
 	     orig init))))
+
+;; load init file
+(let ((init (string-append *cm-root* "lib/cminit.lisp")))
+  (if (file-exists? init)
+    (load init)))
 
 ;;; install cm scripts in bin directory
 
@@ -85,7 +89,7 @@
 ;;                  (first (command-line))
 ;;                  `(begin
 ;;                    (define cm-bin-directory , cm-bin-directory)
-;;                    (load , (string-append cm-directory "src/cm.scm"))
+;;                    (load , (string-append *cm-root* "src/cm.scm"))
 ;;                    (load , (string-append cm-bin-directory
 ;;                                           "cminit.lisp"))
 ;;                    (top-repl)))))
@@ -94,7 +98,7 @@
 ;;        (format #t "~%; Saving startup script: ~S" scr1)
 ;;        (write-cm-script :linux scr1 exec)))
 ;;     (if (not (file-exists? scr2))
-;;       (let ((el (string-append cm-directory
+;;       (let ((el (string-append *cm-root*
 ;;                                "etc/xemacs/listener.el")))
 ;;        (format #t "~%; Saving startup script: ~S" scr2)       
 ;;        (write-xcm-script :linux "guile" scr2 scr1 el)
