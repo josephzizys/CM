91c88e5acda4e12eed0101c513b0f778215b9063
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2004-11-19 19:26:28 +0000

    plotter integration
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@297 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/pkg.lisp
+++ cm/src/pkg.lisp
@@ -29,7 +29,7 @@
 ;;;
 
 (defpackage :cm
-  (:use :common-lisp )
+  (:use :common-lisp #+:gtk :gtk)
   (:shadow :make-load-form
            ;; have to block these from CLM
            :io :ran :exit :quit )
