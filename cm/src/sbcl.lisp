0dccb1fdde6417978ddc66f5db2940e0d32c9e74
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-01-01 02:33:04 +0000

    removed duplicate class-subclasses defun and added defgenerics to sbcl.lisp to stop the fucking style warnings.
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@326 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/sbcl.lisp
+++ cm/src/sbcl.lisp
@@ -24,22 +24,24 @@
           sb-pcl:class-slots
           sb-pcl:class-direct-superclasses
           sb-pcl:generic-function-name
+          sb-mop:class-direct-subclasses
           )
         :cm)
 
 (defun quit () (sb-ext:quit))
 (defun exit () (quit))
 
-(defun class-subclasses (c)
-  (let ((subs (sb-pcl:class-direct-subclasses c)))
-    (if (null subs)
-	'()
-      (loop for s in subs
-	append (cons s (class-subclasses s))))))
+;(defun class-subclasses (c)
+;  (let ((subs (sb-pcl:class-direct-subclasses c)))
+;    (if (null subs)
+;	'()
+;      (loop for s in subs
+;	append (cons s (class-subclasses s))))))
 	  
 (defun finalize-class (class) 
   (sb-pcl:finalize-inheritance class))
 
+(defgeneric validate-class (obj1 obj2))
 (defmethod validate-class ((class t) (superclass t))
   ;; this is a no-op except in OpenMCL 014
   t)
@@ -97,3 +99,106 @@
                                 (cm-logo)
                                 (sb-impl::toplevel-init)
                                 )))
+
+;;
+;;; arrrrg a pox on frigging style warnings!!
+
+
+(defgeneric class-parameters (obj))
+(defgeneric (setf class-parameters) (val obj))
+(defgeneric io-class-file-types (obj))
+(defgeneric (setf io-class-file-types) (val obj))
+(defgeneric io-class-mime-type (obj))
+(defgeneric (setf io-class-mime-type) (val obj))
+(defgeneric io-class-output-hook (obj))
+(defgeneric (setf io-class-output-hook) (val obj))
+(defgeneric io-class-definer (obj))
+(defgeneric (setf io-class-definer) (val obj))
+(defgeneric io-class-file-versions (obj))
+(defgeneric (setf io-class-file-versions) (val obj))
+(defgeneric validate-superclass (obj1 obj2))
+(defgeneric make-load-form (obj))
+(defgeneric fill-object (obj1 obj2))
+(defgeneric rename-object (obj1 name &rest args))
+(defgeneric copy-object (obj))
+(defgeneric subcontainers (obj))
+
+(defgeneric insert-object (obj1 obj2))
+(defgeneric append-object (obj1 obj2))
+(defgeneric remove-object (obj1 obj2))
+(defgeneric remove-subobjects (obj))
+(defgeneric list-subobjects (obj  &key start end start-time end-time))
+(defgeneric amplitude (amp &optional softest loudest power))
+(defgeneric rhythm (rhy &optional tempo beat))
+(defgeneric tuning->mode (mode keynum force?))
+(defgeneric mode->tuning (mode keynum return))
+(defgeneric tuning-hertz->keynum (obj hz))
+(defgeneric tuning-hertz->note (scale hz acci err?))
+(defgeneric tuning-keynum->hertz (obj knum))
+(defgeneric tuning-keynum->note (obj knum acci err?))
+(defgeneric tuning-note->hertz (obj note err?))
+(defgeneric tuning-note->keynum (obj note err?))
+(defgeneric tuning-note->note (obj note acci err?))
+(defgeneric scale-mod (freq modulus &key offset in accidental))
+(defgeneric note-in-scale? (note scale))
+(defgeneric transpose (note int &optional scale))
+(defgeneric invert (ref &optional point scale))
+(defgeneric interval (int &optional int2))
+
+(defgeneric canonicalize-pattern-data (obj data parser inits))
+(defgeneric pattern-external-inits (obj))
+(defgeneric pattern-period-length (obj))
+(defgeneric default-period-length (obj))
+(defgeneric pattern? (obj))
+(defgeneric eop? (obj))
+(defgeneric eod? (obj))
+(defgeneric next-1 (obj))
+(defgeneric skip-datum? (obj))
+(defgeneric reset-period (obj))
+(defgeneric next-in-pattern (obj))
+(defgeneric map-pattern-data (fn obj))
+
+(defgeneric io-handler-args (obj))
+(defgeneric io-handler-args? (obj))
+(defgeneric set-io-handler-args! (obj args))
+(defgeneric init-io (obj &rest inits))
+(defgeneric open-io (obj dir &rest args))
+(defgeneric close-io (obj &rest mode))
+(defgeneric initialize-io (obj))
+(defgeneric deinitialize-io (obj))
+(defgeneric write-event (obj io time))
+(defgeneric import-events (obj &rest args))
+
+(defgeneric schedule-object (obj start))
+(defgeneric unschedule-object (obj &rest recurse))
+(defgeneric process-events (obj time start func))
+(defgeneric sprout (obj &optional time))
+
+(defgeneric midi-event-data1 (obj))
+(defgeneric midi-event-data2 (obj))
+(defgeneric midi-event->midi-message (obj))
+
+(defgeneric midi-write-message (msg mf time data))
+
+(defgeneric object->midi (obj))
+
+(defgeneric midishare-stream-refnum (obj))
+(defgeneric open-midishare-client (obj name))
+(defgeneric close-midishare-client (obj))
+
+(defgeneric player-play (obj))
+(defgeneric player-set-sync (obj sync))
+(defgeneric player-set-tempo (obj tempo))
+(defgeneric player-start (obj))
+(defgeneric player-stop (obj))
+(defgeneric player-pause (obj))
+(defgeneric player-cont (obj))
+(defgeneric player-mute (obj track))
+(defgeneric player-unmute (obj track))
+(defgeneric player-solo (obj track))
+(defgeneric player-unsolo (obj track))
+(defgeneric player-load-midifile (obj fil))
+(defgeneric player-save-midifile (obj fil))
+
+(defgeneric object->cmn (obj))
+
