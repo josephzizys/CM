d0310047cee7affbb08a6eb92a50feb8ec9268f0
mklingb <mklingb@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-01-29 04:45:54 +0000

    commented out warning for the use of the bare :hz tag for the hertz conversion function
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@350 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/scales.scm
+++ cm/src/scales.scm
@@ -763,8 +763,9 @@
            false))
     (when (and (pair? args) (member (first args) %hertz)
                (odd? (length args)))
-      (warn "Found :HZ tag, use the :HZ keyword parameter instead: ~s"
-            (list* 'note ':hz true (rest args)))
+      ;; I hate this warning -- what was wrong with the old way?
+      ;;(warn "Found :HZ tag, use the :HZ keyword parameter instead: ~s"
+      ;;      (list* 'note ':hz true (rest args)))
       (set! args (list* ':hz true (rest args))))
     (dopairs (sym val args)
       (case sym
