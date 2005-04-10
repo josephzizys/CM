1fc13ddde7aab9bd36fd2745501bdc2e3bbefba1
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-04-10 23:06:17 +0000

    bumped version to 2.7.0
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@465 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/utils.scm
+++ cm/src/utils.scm
@@ -347,7 +347,7 @@
 ;;;   minor:       backwards-compatible API change, i.e. "feature-add"
 ;;;   maintenance: no API change, bug fix only.
 
-(define %cm-version% #x260)
+(define %cm-version% #x270)
 
 (define (cm-version-number)
   (format #f "~a.~a.~a" 
