0c7c3e30bb7071dbb343142b4e81b6d7cdfa0433
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-04-25 21:34:11 +0000

    converted to unix eol, renamed functionp to procedure? and added copyright notice
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@489 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/sc.scm
+++ cm/src/sc.scm
@@ -1,3 +1,22 @@
+;;; **********************************************************************
+;;; Copyright (C) 2005 Todd Ingalls, Heinrich Taube
+;;; 
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; **********************************************************************
+
+;;; $Name$
+;;; $Revision$
+;;; $Date$
+
 ;;; need to set to the directory where scsynth lives
 
 (define *sc-directory* "/Applications/SuperCollider3/")
@@ -23,7 +42,7 @@
   (next-method))
 
 (define (set-sc-output-hook! fn)
-  (unless (or (not fn) (functionp fn))
+  (unless (or (not fn) (procedure? fn))
     (error "Not a supercollider file hook: ~s" fn))
   (set! (io-class-output-hook <sc-file-stream>) fn)
   (values))
@@ -39,7 +58,7 @@
   (values))
 
 (define (set-sc-output-hook! fn)
-  (unless (or (not fn) (functionp fn))
+  (unless (or (not fn) (procedure? fn))
     (error "Not a supercollider file hook: ~s" fn))
   (set! (io-class-output-hook <sc-file-stream>) fn)
   (values))
