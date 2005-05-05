1979e3487456693f960e23b6c61cd19491e2b6ee
testcase <testcase@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-05-05 03:53:28 +0000

    fixed incorrect loop keyword in u8vector->string
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@536 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/utils.scm
+++ cm/src/utils.scm
@@ -540,7 +540,7 @@
         (if (exact? val) (exact->inexact val) val)))))
 
 (define (u8vector->string vec)
-  (list->string (loop for i across (u8vector-length vec)
+  (list->string (loop for i across vec
                    until (= i 0)
                    collect (integer->char i))))
 
