0d9324f69066ca7c90b3bdb4244abce6f5f30814
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-05-16 17:08:10 +0000

    removed define in illegal cond-expand (not at toplevel)
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@581 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/cm.scm
+++ cm/src/cm.scm
@@ -9,61 +9,67 @@
       (file-list '("loop" "level1" "utils" "mop" "objects"
                    "io" "scheduler" "sco" "clm" "clm2"
                    "midi1" "midi2" "midi3" "data" "scales" "spectral"
-                   "patterns" "osc" "sc")))
+                   "patterns" "osc"
+                   "sc"
+                   )))
   (cond-expand
    (guile
     (set! this-file (port-filename (current-load-port)))
     (set! file-list (cons "guile" file-list)))
    (gauche
-    (define (force-output) #f)
     (set! this-file (port-name (current-load-port)))
     (set! file-list (cons "gauche" file-list)))
-;   (chicken
-;    (set! this-file 
-;          (eval (with-input-from-string "##sys#current-load-file" read)))
-;    (set! file-list (list "chicken" "loop"))
-;    (load-verbose #f))
+;;  (chicken
+;;   (set! this-file 
+;;   (eval (with-input-from-string "##sys#current-load-file" read)))
+;;   (set! file-list (list "chicken" "loop"))
+;;   (load-verbose #f))
    (stklos
-    #f)
-   )
+    (set! this-file "cm.scm")
+    (set! file-list (cons "stklos" file-list))))
   
   (do ((last-slash #f)
        (i 0 (+ i 1))
        (l (string-length this-file)))
       ((= i l) 
        (if last-slash
-         (set! load-path (substring this-file 0 (+ last-slash 1)))))
+           (set! load-path (substring this-file 0 (+ last-slash 1)))))
     (if (char=? (string-ref this-file i) #\/)
-      (set! last-slash i)))
+        (set! last-slash i)))
 
-   (cond-expand
-    (guile
-     (set! %load-path (cons load-path %load-path))
-     (set! load-path "")
-     )
-    (gauche 
-     (set! *load-path* (cons load-path *load-path*))
-     (set! load-path "")
-     )
+  (cond-expand
+   (guile
+    (set! %load-path (cons load-path %load-path))
+    (set! load-path ""))
+   (gauche 
+    (set! *load-path* (cons load-path *load-path*))
+    (set! load-path ""))
+   (stklos
+    ;;(set! *load-path* (cons load-path *load-path*))
+    ;;(set! load-path "")
     )
+   )
 
-   (let loadem ((tail file-list))
-        (cond ((null? tail) #f)
-              (else
-               (let ((file (string-append load-path (car tail) ".scm")))
-                 (display (string-append "; loading " file))
-                 (force-output)
-                 (newline)
-                 (load file)
-                 (loadem (cdr tail))
-                 ))))
-   ;; load user init file if it exists
-   (let* ((this (pwd))
-          (home (cd))
-          (init (string-append home "/.cminit.lisp")))
-     (if (file-exists? init)
-       (load init))
-     (cd this))
+  (let loadem ((tail file-list))
+       (if (null? tail) 
+           #f
+           (let ((file (string-append load-path (car tail) ".scm")))
+             (display (string-append "; loading " file))
+             (cond-expand
+              (guile (force-output))
+              (else #f))
+             (newline)
+             (load file)
+             (loadem (cdr tail))
+             )))
 
-   ) ; end let
+  ;; load user init file if it exists
+  (let* ((this (pwd))
+         (home (cd))
+         (init (string-append home "/.cminit.lisp")))
+    (if (file-exists? init)
+        (load init))
+    (cd this))
+
+  )                                     ; end let
 
