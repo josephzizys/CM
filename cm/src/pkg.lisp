02b1ccce9d14967d9caeae7753b5fb6c71e24ab8
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-06-03 19:56:36 +0000

    export cm symbolds defined in dictionary
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@614 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/pkg.lisp
+++ cm/src/pkg.lisp
@@ -97,29 +97,52 @@
 		:TicksPerQuarterNote
 		:SetSynchroInPlayer :MidiNewMidiFileInfos
 		:MidiFileLoad :MidiFileSave :mf-clicks :mf-format :mf-timedef)
-  (:export
-
-   ;; data.lisp
-   :interval :decode-interval :*tempo* :*beat* :rhythm :*softest* :*loudest*
-   :*power* :amplitude :interpl :interp :tendency :rescale-envelope :quantize
-   :rescale :fit :cents->scaler :scaler->cents :between :pick :pickl :vary
-   :drunk :odds :shuffle :ran :expl :explsegs :explseg :geosegs :geoseg
-   :best-normal-form :prime-form :markov-analyze :histogram
-
-   ;; objects.lisp
-   :copy-object :fill-object :*print-instance* :print-instance :save-object
-   :object-name :object-time :rename-object :list-named-object :find-object
-   :container :seq :subobjects :subcontainers :map-subobjects 
-   :map-subcontainers :insert-object :append-object :remove-object
-   :list-subobjects :new :event :*time-slots* :defobject :process :defprocess
-
-   ;; scales.lisp
-   :tuning :mode :*scale* :*chromatic-scale* :rest?
-   :note :keynum :hertz :transpose :invert :scale=
-   :scale< :scale> :scale<= :scale>= :interval
-
+  
+  (:export :accumulation :amplitude :append-object :audio-file :axis
+           :*beat* :best-normal-form :between :cd :cents->scaler
+           :chord :*chromatic-scale* :clm-file :cm :cm-version :cmio
+           :cmn :cmn-file :copier :copy-object :cycle :date-and-time
+           :decimals :decode-interval :defaxis :defobject :defprocess
+           :doeach :drunk :dumposc :eod? :eop? :events :expl :explseg
+           :explsegs :f :false :find-object :fit :fm-spectrum
+           :fold-objects :funcall :graph :heap :hertz :histogram :i
+           :import-events :input :insert-object :interp :interpl
+           :interval :invert :io :join :keynum :line
+           :list-named-objects :list-subobjects :log-axis :lookup
+           :*loudest* :make-cm :map-objects :map-pattern-data
+           :map-subcontainers :map-subobjects :markov-analyze :markov
+           :midi-chan-event :midi-channel-map :midi-channel-pressure
+           :midi :midi-connections :midi-control-change :midi-eot
+           :midi-file :midi-file-print :midi-key-pressure
+           :midi-key-signature :midi-note-off :midi-note-on
+           :midi-pitch-bend :midi-port-event :midi-program-change
+           :midi-sequence-number :midi-sequencer-event
+           :midi-smpte-offset :midi-stream :midi-system-event
+           :midi-tempo-change :midi-text-event :midi-time-signature
+           :midishare-stream :mode :new :next :note-accidental :note
+           :note-name :now :object->cmn :object-name
+           :object-parameters :object-time :octave-number :odds
+           :output :palindrome :pattern-state :pattern-value :pattern?
+           :pick :pickl :pitch-class :play :player-cont
+           :player-load-midifile :player-mute :player-pause
+           :player-save-midifile :player-set-tempo :player-solo
+           :player-start :player-stop :player-stream :player-unmute
+           :player-unsolo :plotter-add-layer :plotter-close
+           :plotter-data :plotter :plotter-front-styling
+           :plotter-property :plotter-redraw :plotter-scroll
+           :plotter-zoom :point :power :prime-form :process :pval
+           :pval :pwd :quantize :ran :random :range :ransegs
+           :remove-object :remove-subobjects :rescale-envelope
+           :rescale :rewrite :rewrite-generation :rhythm :rm-spectrum
+           :rotation :save-object :sc-file :scale-max :scale-min
+           :scale-mod :scale-order :*scale* :scale= :scale> :scale>=
+           :scale< :scale<= :scaler->cents :sco-file :seq
+           :set-clm-output-hook! :set-midi-output-hook!
+           :set-sco-output-hook! :shell :shuffle :*softest* :sprout
+           :stop :subcontainers :subobjects :sv :sv+ :sv* :*tempo*
+           :tendency :*time-slots* :transpose :transposer :true
+           :tuning :vary :wait :wait-until)
   )
-)
 
 ;;;
 ;;; intern and export ms:new and ms:MidiPrintEv
