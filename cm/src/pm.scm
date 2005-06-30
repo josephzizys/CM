fe6971cf8cf6556a7e43edd8e1ea7e2f7ed2a6a5
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-06-30 03:25:51 +0000

    added <midi> object and non-realtime output
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@654 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/pm.scm
+++ cm/src/pm.scm
@@ -22,7 +22,9 @@
    (bufsize :init-value 256 :init-keyword :bufsize
             :accessor portmidi-bufsize)
    (receiver :init-value #f :init-keyword :receiver
-             :accessor portmidi-receiver))
+             :accessor portmidi-receiver)
+   (offset :init-value 0 :init-keyword :offset 
+           :accessor portmidi-offset))
   :name 'portmidi-stream
   :metaclass <io-class>
   :file-types '("*.pm"))
@@ -47,7 +49,7 @@
                                (third l)
                                #f))
                           ((integer? i)
-                           (if (eql (third l) i)
+                           (if (eq? (third l) i)
                                (third l)
                                #f))
                           (else #f))))
@@ -62,7 +64,7 @@
           (if (and i (not idev))
               (set! idev ( getd i ':input (car tail))))
           (if (and o (not odev))
-              (set! odev ( getd i ':output (car tail)))))
+              (set! odev ( getd o ':output (car tail)))))
         (pm:TimerStart)
         (set! (object-time obj) 0)
         (when idev
@@ -91,61 +93,140 @@
   (values))
 
 (define-method* (initialize-io (obj <portmidi-stream>))
-  ;; cache current time offset
-  (set! (object-time obj) (pm:TimerTime))
+  ;; this method will have to distinguish between rt and non-rt
+  ;; scheduling and also input vs output.
+
+  ;;cache current MILLISECOND time offset
+  (set! (portmidi-offset obj) (pm:TimerTime))
+  ;; stream's time is kept in seconds. maybe stream should have a
+  ;; scaler like midi-file so user could work with whatever quanta
+  ;; they wanted...
+  (set! (object-time obj) 0)
   (channel-tuning-init obj))
+
+;; THIS SHOULD BE MERGED WITH 
+(define (flush-pending-offs2 mf at)
+  (let ((time (or at most-positive-fixnum)))
+    (do ((qe (%q-peek %offs) (%q-peek %offs)))
+	((or (null? qe) (> (%qe-time qe) time))
+	 #f)
+      (%q-pop %offs)      
+;      (format t "~%flushing ~S" (%qe-time qe))
+      (midi-write-message (%qe-object qe) 
+			  mf
+                          (%qe-time qe)
+			  #f)
+      (%qe-dealloc %offs qe))))
 
 (define-method* (deinitialize-io (obj <portmidi-stream>))
   ;; uncache current time offset
-  (set! (object-time obj) 0))
+  (flush-pending-offs2 obj #f))
+
+;;; midi.pm
 
 (define (portmidi-open . args)
-  (apply #'open-io "midiport.pm" #t args))
+  (apply #'open-io "midi.pm" #t args))
 
-(define (portmidi-open? io)
-  (if (io-open io) #t #f))
+(define (portmidi-open? . args)
+  (with-args (args &optional (port (find-object "midi.pm" )))
+    (if (and port (io-open port))
+        #t
+        #f)))
 
-(define-method* (write-event (obj <integer> ) (str <portmidi-stream>)
-                             scoretime)
+(define (portmidi-close . args)
+  (with-args (args &optional (port (find-object "midi.pm" )))
+    (if (portmidi-open? port)
+        (begin (close-io port ':force) #t)
+        #f)))
+
+(define-method* (midi-write-message (obj <integer> )
+                                    (str <portmidi-stream>)
+                                    scoretime data)
+  data
   (cond ((sysex-p obj)
          ;; add in later..
          )
         ((or (channel-message-p obj)
              (system-message-p obj))
+;         (terpri)
+;         (midi-print-message 
+;          obj (inexact->exact (round (* scoretime 1000))))
          (pm:WriteShort 
           (second (io-open str))        ; output stream
+          ;; add in time offset in stream
           (+ (inexact->exact (round (* scoretime 1000)))
-             (object-time str))
+             (portmidi-offset str))
           (pm:Message (logior (ash (midimsg-upper-status obj) 4)
                               (midimsg-lower-status obj))
                       (channel-message-data1 obj)
-                      (channel-message-data2 obj))))
+                      (channel-message-data2 obj)))
+         )
         (else #f)))
 
-;;;
-;;; portmidi behaves almost like a midi-file in that it (1) handles
-;;; only true midi data (ie no durations); (2) data must be sent in
-;;; time-increasing order; (3) messages are really just bytes. so it
-;;; would be nice if there were a single stream that handled this. the
-;;; essential difference between portmidi and midi-files is that times
-;;; in midi-files are deltas but are absolute in portmidi.
+;;; portmidi behaves almost like a midi-file: (1) handles only true
+;;; midi messages (ie no durations); (2) data must be sent over in
+;;; time-increasing order; (3) messages are just bytes. the only real
+;;; difference between outputting to portmidi vs midi-files is that
+;;; times in midi-files are deltas but portmidi uses absolute.
 
 (define-method* (write-event (obj <midi> ) (str <portmidi-stream>)
                              scoretime)
-  obj str scoretime
-  )
+  (let ((keyn (midi-keynum obj))
+        (chan (midi-channel obj))
+        (ampl (midi-amplitude obj)))
+    ;; if amplitude is zero then don't output anything
+    (ensure-velocity ampl keyn)
+    (ensure-microtuning keyn chan str)
+    ;; if "resting" then dont update anything
+    (unless (< keyn 0)                  ; rest
+      (unless (null? (%q-head %offs))
+        (flush-pending-offs2 str scoretime))
+      (midi-write-message (make-note-on chan keyn ampl)
+                          str
+                          scoretime
+                          #f)
+      (%q-insert (%qe-alloc %offs
+                            (+ scoretime (midi-duration obj))
+                            #f
+                            (make-note-off chan keyn 127))
+                 %offs))
+    (values)))
 
 (define-method* (write-event (obj <midi-event>) (str <portmidi-stream>)
                              scoretime)
-  (write-event (midi-event->midi-message obj) str scoretime))
+  ;; flush here too?
+  (unless (null? (%q-head %offs))
+    (flush-pending-offs2 str scoretime))
+  (midi-write-message (midi-event->midi-message obj) str scoretime #f))
+
+(define-method* (write-event (obj <integer>) (str <portmidi-stream>)
+                             scoretime)
+  ;; flush here too?
+  (unless (null? (%q-head %offs))
+    (flush-pending-offs2 str scoretime))
+  (midi-write-message obj str scoretime #f) )
 
 
+;;; real-time support. have to think about this...
 
+(define-method* (rt-output ev (out <portmidi-stream>) . args)
+  ;; output ev to Midishare
+  (with-args (args &optional ahead)
+    (if ahead
+      (write-event ev out (+ ahead (pm:TimerTime)))
+      (write-event ev out 0))
+    (values)))
 
+(define-method* (rt-now (io <portmidi-stream>))
+  (pm:TimerTime))
 
+;;; (pm:GetDeviceDescriptions)
+;;; (setq foo (portmidi-open :input nil :output 3))
+;;; (events (loop for d from 0 to 8 by .5
+;;;             for k from 60
+;;;             collect (new midi :time d :duration 1 
+;;;                          :keynum k
+;;;                          :amplitude .9))
+;;;        foo)
 
-; (setq foo (portmidi-open ))
-; (describe foo)
-; (write-event (make-note-on 0 60 100) foo 0)
-; (write-event (make-note-off 0 60 100) foo 0)
 
