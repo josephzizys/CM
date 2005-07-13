a4adb5d7e290d5c23a87e72d75fb68a5369e99a0
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-07-13 02:25:06 +0000

    rts uses a current time to try to limit effect of time jitter. new api thead-current-time used to shield implementation details. removed code associated with processes as threads --  that code as well as the original version of rts and the example code at the end of the file can be gotten from rev 1.9 of this file.
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@710 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/rt.scm
+++ cm/src/rt.scm
@@ -18,101 +18,8 @@
 ;;; $Date$
 
 ;;;
-;;; this file contains experimental code for attempting "realtime"
-;;; processes and scheduling based on the srfi-18 multithreading
-;;; api: http://srfi.schemers.org/srfi-18/
-;;;
-;;;
-;;; model 1: processes as threads
-;;;
-
-(define *threads* '())
-
-(define (thread-alloc . args)
-  (apply (function make-thread) args))
-
-(define (thread-dealloc th)
-  th
-  (values))
-
-(define (rt-events object to . args)
-  (let ((proc-list '())
-        (ahead (if (and (pair? args)
-			(or (pair? (car args))
-			    (number? (car args))))
-                 (pop args)
-                 0)))
-    (when (odd? (length args))
-      (err "Uneven initialization list: ~s." args))
-    (when to
-      (set! *out* (open-io (apply (function init-io) to args)
-                           ':output))
-      (initialize-io *out*))
-    (if ahead
-        (if (list? ahead)
-            (dolist (obj object)
-              (sprout obj (pop ahead) *out*))
-          (push (sprout object ahead) proc-list)))
-    proc-list))
-
-(define-method* (rt-now io)
-  io
-  0)
-
-(define-method* (rt-wait delta io)
-  io
-  (let ((start (time->seconds (current-time))))
-    (thread-sleep! (seconds->time (+ delta start)))))
-
-(define-method* (rt-output event out  . args)
-  (with-args (args &optional ahead)
-    ahead
-    (write-event event out (object-time event))
-    (values)))
-
-(define-method* (rt-sprout (obj <object>) ahead out)
-  (set! *out* out)
-  (if (and ahead (not (= ahead 0)))
-      (schedule-ahead (output obj out) ahead)
-    (output obj out)))
-
-(define-method* (rt-sprout (obj <thread>) ahead out)
-  ahead out
-  (thread-start! obj))
-
-(define-method* (rt-sprout (obj <procedure>) ahead out)
-  ahead
-  (set! *out* out)
-  (let ((proc (thread-alloc
-               (lambda ()
-                 (loop until
-                       (not (funcall obj)))))))
-    (thread-start! proc)
-    proc))
-
-(define (schedule-ahead thunk ahead)
-  (let ((thread (thread-alloc
-                 (lambda ()
-                   (begin
-                     (thread-sleep! ahead)
-                     (funcall thunk))))))
-    (thread-start! thread)))
-
-(define-macro (thread . body)
-  (let ((fn (gensym))
-        (th (gensym))
-        (ok (gensym)))
-    `(let ((,th (thread-alloc
-                 (lambda ()
-                   (let ((fn , (expand-process body *process-operators*)))
-                     (do ((,ok (funcall , fn ))) 
-                         ((not ,ok)
-                          (thread-dealloc ,th))
-                       (set! ,ok (funcall , fn ))))))))
-       ,th)))
-
-;;;
-;;; model 2: scheduler as thread
+;;; rts real time scheduler. attempts to be adaptive to fluctuations
+;;; using a targettime to calculate sleep time
 ;;;
 
 (define *rts-run* #f)
@@ -126,27 +33,40 @@
   (set! *scheduler* #f)
   (set! *rts-run* #f)
   (set! *rts-now* #f))
-(define (rts-running? ) *rts-run*)
-(define (rts-now ) *rts-now*)
-(define (rts-stop) (set! *rts-run* #f))
-(define (rts-pause ) (err "rts-pause: not implemented."))
-(define (rts-continue ) (err "rts-continue: not implemented."))
 
-(define (rts object to . at)
+(define (rts-running? )
+  *rts-run*)
+
+(define (rts-now )
+  *rts-now*)
+
+(define (rts-stop) 
+  (set! *rts-run* #f))
+
+(define (rts-pause )
+  (err "rts-pause: not implemented."))
+
+(define (rts-continue )
+  (err "rts-continue: not implemented."))
+
+(define (rts object to . args)
   (if (rts-running?) (err "rts: already running."))
-  (if (not *rts-lock*) (set! *rts-lock* (make-mutex)))
-  (let ((ahead (if (pair? at) (car at) 0)))
+  (if (not *rts-lock*)
+      (set! *rts-lock* (make-mutex)))
+  (let* ((ahead (if (pair? args) (pop args) 0))   ; optional start time
+         (endat (if (pair? args) (pop args) #f))  ; optional end time
+         (ttime #f))  ; target time tries to control time disturbances
     (set! *queue* %q)
     (set! *scheduler* #t)
-    (if (consp object)
+    (if (pair? object)
         (dolist (o object)
           (schedule-object o
-                           (if (consp ahead)
-                               (if (consp (cdr ahead))
+                           (if (pair? ahead)
+                               (if (pair? (cdr ahead))
                                    (pop ahead)
                                    (car ahead))
                                ahead)))
-        (if (consp ahead)
+        (if (pair? ahead)
             (schedule-object object (car ahead))
             (schedule-object object ahead)))
     (set! *out* to)
@@ -154,27 +74,31 @@
     ;; not sure about this
     (if (pair? ahead) (set! ahead (apply (function min) ahead)))
     (set! *rts-now* ahead)
+    (setf ttime (+ *rts-now* (thread-current-time)))
     (thread-start!
-     (thread-alloc
+     (make-thread
       (lambda ()
-        (if (> *rts-now* 0) (thread-sleep! *rts-now*))
+        (if (> *rts-now* 0) 
+            (thread-sleep! *rts-now*))
         (do ((entry #f)
              (qtime #f)
              (start #f)
              (thing #f)
              (wait? #f)
              (none? (null? (%q-head *queue*))))
-            ((or none? (not *rts-run*))
+            ((or none? (not *rts-run*)
+                 (and endat (> (%qe-time (%q-peek *queue*)) endat)))
              (unless none? (%q-flush *queue*))
              (unschedule-object object #t)
              (set! *rts-run* #f)
              (set! *queue* #f)
              (set! *scheduler* #f)
              (set! *rts-now* #f))
-          (mutex-lock! *rts-lock*) ; is this necessary? i think with
+          (mutex-lock! *rts-lock*)   ; is this necessary? i think with
           (without-interrupts
               (do ()
-                  ((or none? (> (%qe-time (%q-peek *queue*)) *rts-now*))
+                  ((or none? 
+                       (> (%qe-time (%q-peek *queue*)) *rts-now*))
                    (if none?
                        (set! wait? #f)
                        (let* ((next (%qe-time (%q-peek *queue*))))
@@ -186,13 +110,13 @@
                 (set! thing (%qe-object entry))
                 (%qe-dealloc *queue* entry)
                 (process-events thing qtime start *out*)
-                (set! none? (null? (%q-head *queue*)))
-                ))
-          (mutex-unlock! *rts-lock*) ; is this necessary?
-          (if wait? (thread-nanosleep! wait?))
-          ))))))
-
-;; 
+                (set! none? (null? (%q-head *queue*)))))
+          (mutex-unlock! *rts-lock*)    ; is this necessary?
+          (if wait? 
+              (begin 
+               (setf ttime (+ ttime wait?))
+               (thread-sleep!
+                (- ttime (thread-current-time)))))))))))
 
 (define-method* (rts-sprout obj &key to at ahead)
   ;; im not sure if i need a locking mechanism here or not. rts wraps
@@ -212,37 +136,6 @@
           ))
   (mutex-unlock! *rts-lock*)
   (values))
-
-;;;
-;;; tests
-
-;(defparameter *pm*
-;  (portmidi-open :input nil :output 3 :latency 0))
-;
-;(define (zzz len lb ub wai amp)
-;  (process repeat len
-;    output (new midi :time 0
-;            :duration .1 :amplitude amp
-;            :keynum (between lb ub))
-;    wait wai))
-;
-;; fire it up
-;(rts (list (zzz 100 60 90 .25 .5)
-;           (zzz 50 20 50 .5 .5))
-;     *pm* 0)
-;
-;; now eval this whenever...
-;(let ((k (between 20 100))
-;      (n (pick 3 5 7 11)))
-;  (rts-sprout (zzz (* n (pick 2 3 4)) (+ k 7) (/ 1 n) .75) :ahead 0))
-
-
-
-
-
-
-
-
 
 
 
