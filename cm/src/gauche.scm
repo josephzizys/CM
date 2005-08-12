13818acf84f05668b5ab3c11bb2253b224d44ad9
testcase <testcase@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-08-12 03:14:52 +0000

    added osc to gauche
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@817 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/gauche.scm
+++ cm/src/gauche.scm
@@ -27,7 +27,7 @@
 (use gauche.threads) ; for rt threads
 (use gauche.net) ; needed for socket communication
 (use gauche.fcntl) ;need to set socket to non-blocking
-
+(use gauche.sequence)
 
 ;; Lisp environment normalization
 
@@ -354,7 +354,8 @@
     (lambda (thread)
       (eq? (thread-join! thread 0 unique) unique))))
 
-
+(define (thread-current-time )
+  (time->seconds (current-time)))
 ;;;
 ;;; socket/udp support
 ;;;
@@ -363,6 +364,23 @@
 ;; i want to encapsulate the remote host and port
 ;; information along with the socket.
 ;; udp-socket class does this .
+;;
+;; functions/methods needed
+;; (make-udp-socket host port local-port) x
+;; (udp-socket-close sock) x
+;; (udp-socket-shutdown sock &optional) x
+;; (udp-socket-recv sock bytes &optional flags) x
+;; (udp-socket-send sock mess len) x
+;; (send-osc mess osc-stream len) x
+;; (make-osc-timetag offset out) x
+;; (u8vector->double vec) 
+;; (osc-bundle? vec) x
+;; (osc-parse-timestamp vec) 
+;; (osc-parse-contents vec) x
+;; (osc-parse-message vec) x
+;; (osc-parse-bundle vec) x
+;; (osc-vector->osc-message vec) x
+;;
 
 (define-class* <udp-socket> ()
   ((socket :init-value #f)
@@ -426,49 +444,62 @@
    (udp-socket-send (slot-ref osc-stream 'socket) mess len))
 
 
+(define (make-osc-timetag offset out)
+  (let* ((now (current-time))
+         (offset-time (seconds->time offset))
+         (target-time (seconds->time (+ (time->seconds now) (time->seconds offset-time) (slot-ref out 'latency))))
+         (vec #f))
+    (set! vec (make-byte-vector (+ 2208988800 (slot-ref target-time 'second))))
+    (u8vector-append vec (make-byte-vector (inexact->exact (* (modf (time->seconds target-time)) #xffffffff))))))
 
-
-
-
-
-;; (define (make-osc-timetag offset out)
-;;   (let* ((now (current-time))
-;;          (offset-time (seconds->time offset))
-;;          (target-time (seconds->time (+ (time->seconds now) (time->seconds offset-time) (slot-ref out 'latency))))
-;;          (vec #f))
-;;     (set! vec (make-byte-vector (+ 2208988800 (slot-ref target-time 'second))))
-;;     (u8vector-append vec (make-byte-vector (inexact->exact (* (modf (time->seconds target-time)) #xffffffff))))))
-
-;;;
-;;; not in utils.scm because i think this
-;;; will wind up being implementation
-;;; dependent. - tmi
 
 (define (u8vector->double vec)
   (let ((dv (uvector-alias <f64vector> vec)))
     (f64vector-ref dv 0)))
 
+(define *bundle-header-bytes* #u8(35 98 117 110 100 108 101 0))
 
-;;this is broken now
-(define (parse-osc str)
+(define (osc-bundle? vec)
+  (let ((res #t))
+    (if (u8vector? vec)
+        (dotimes (i 8)
+          (when (not (= (u8vector-ref vec i) (u8vector-ref *bundle-header-bytes* i)))
+            (set! res #f)))
+      (set! res #f))
+    res))
+          
+(define (osc-parse-timestamp vec)
+  (let ((ts #f))
+    (setf ts (u8vector->uint (u8vector-subseq arr 0 4)))
+    (setf ts (+ ts
+             (exact->inexact
+              (/ (u8vector->uint (u8vector-subseq arr 4 8))
+                 4294967295))))))
+
+(find-index (lambda (x) (= x 0)) *bundle-header-bytes*)
+
+(define (osc-parse-contents arr)
   (let ((lst (list))
+	(mess #f)
 	(pos #f)
-        (first-token-len #f)
-        (sym-vector #f)
-        (sym #f)
-        (sym-len 0)
-	(type-list (list))
-        (type-list-len 0))
-    (set! first-token-len (string-scan str "\0"))
+	(sym-vector #f)
+	(sym #f)
+	(sym-len 0)
+	(first-token-len #f)
+	(type-list #f)
+	(type-list-len #f))
+    (set! first-token-len (find-index (lambda (x) (= x 0)) arr))
     (if first-token-len
-        (let ((mess #f))
-          (set! lst (append! lst (list (string->symbol (string-trim (substring str 0 first-token-len) #\/)))))
-          (set! mess (string-drop str (string-scan str ",")))
-          (set! type-list (string->list (string-incomplete->complete (substring mess 0 (string-scan mess "\0")))))
-          (set! type-list-len (length type-list))
-          (set! pos (+ type-list-len (- 4 (mod type-list-len 4))))
-          (set! mess (string->u8vector mess))
-          (dolist (j type-list)
+	(begin
+	  (set! lst (append! lst (list (string->symbol
+                                        (string-upcase (u8vector->string (u8vector-subseq arr 0 first-token-len)))))))
+	  (set! mess (u8vector-subseq arr (find-index (lambda (x) (= x 44))  arr)))
+	  (set! type-list (loop for i across (subseq mess 0 (position 0 mess))
+				collect (code-char i)))
+	  (set! type-list-len (length type-list))
+	  (set! mess (u8vector-subseq mess (+ type-list-len (- 4 (mod type-list-len 4)))))
+	  (set! pos 0)
+	  (dolist (j type-list)
             (cond ((eq? j #\i)
                    (set! lst (append! lst (list (u8vector->int (u8vector-subseq mess pos (+ pos 4))))))
                    (set! pos (+ pos 4)))
@@ -481,11 +512,36 @@
                   ((eq? j #\s)
                    (set! sym-vector (u8vector-subseq mess pos))
                    (set! sym (u8vector->string sym-vector))
-                   (set! lst (append! lst (list (string-trim (string-trim-right sym #\newline) #\/))))
+                   (set! lst (append! lst (list (string-trim-right sym #\newline) #\/)))
                    (set! sym-len (string-length sym))
                    (if (= 0 (mod sym-len 4))
                        (set! pos (+ pos sym-len))
                      (set! pos (+ (+ sym-len (- 4 (mod sym-len 4))) pos)))))))
-      (set! lst (list (string->symbol str))))
+      (set! lst (append! lst (list (string->symbol (u8vector->string arr))))))
     lst))
 
+(define (osc-parse-message vec)
+  (osc-parse-contents vec))
+
+(define (osc-parse-bundle arr)
+  (let ((msg-len #f)
+        (pos 0)
+        (arr-len (u8vector-length arr))
+        (bundle (list)))
+    (do ()
+        ((>= pos arr-len))
+      (set! msg-len (u8vector->int (u8vector-subseq arr pos 4)))
+      (set! pos (+ pos 4))
+      (setf bundle (append! bundle (osc-parse-contents (u8vector-subseq arr pos (+ pos msg-len)))))
+      (set! pos (+ pos msg-len)))
+    bundle))
+
+(define (osc-vector->osc-message arr)
+  (let ((timestamp #f)
+	(msg #f))
+    (if (osc-bundle? arr)
+	(begin 
+	  (set! timestamp (osc-parse-timestamp (u8vector-subseq arr 8 16)))
+	  (set! msg (osc-parse-bundle (u8vector-subseq arr 16))))
+      (set! msg (osc-parse-contents arr)))
+    (list msg timestamp)))
