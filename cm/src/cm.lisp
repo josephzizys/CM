e38ed523ede7a194824348e8b0580cc99fc3995e
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-11-21 15:31:44 +0000

    switched over to load-system
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@922 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/cm.lisp
+++ cm/src/cm.lisp
@@ -2,18 +2,10 @@
 
 (in-package :cl-user)
 
-;(load (merge-pathnames "make.lisp" *load-pathname*) :verbose nil)
-;(make-cm)
+(load 
+ (make-pathname :name "cm" :type "asd"
+                :directory (butlast (pathname-directory *load-pathname*))
+                :defaults *load-pathname*))
 
-(let (;(*load-verbose* nil)
-      ;(*load-print* nil)
-      (file (make-pathname :name "cm" :type "asd"
-                           :directory
-                           (butlast
-                            (pathname-directory *load-pathname*))
-                           :defaults *load-pathname*)))
-  (load file)
-  (funcall (find-symbol "OPERATE" :asdf)
-           (find-symbol "LOAD-OP" :asdf)
-           :cm))
+(load-system :cm)
 
