94b3e56fb6e8bd0f47b61e8f1143cf552f398aa5
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2005-11-21 15:39:57 +0000

    flipped file eol from dos format to unix
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@923 d60aafaf-7936-0410-ba4d-d307febf7868

--- cm/src/cm.lisp
+++ cm/src/cm.lisp
@@ -2,10 +2,13 @@
 
 (in-package :cl-user)
 
+
 (load 
  (make-pathname :name "cm" :type "asd"
                 :directory (butlast (pathname-directory *load-pathname*))
                 :defaults *load-pathname*))
 
+
 (load-system :cm)
+
 
