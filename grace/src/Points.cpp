8b5c46b7162a999a72226b3e1e862d3e99f87ff6
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2007-09-24 18:35:50 +0000

    added more code for pointdefs pointfields and soundmaps
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@1403 d60aafaf-7936-0410-ba4d-d307febf7868

--- grace/src/Points.cpp
+++ grace/src/Points.cpp
@@ -31,3 +31,55 @@ XmlElement * PointField::toXml() {
   top->addChildElement(val);
   return top;
 }
+
+float SoundParameter::getValue(float *point) {
+  switch (mode) {
+  case FXX:
+    return point[F[V]] ;
+    break;
+  case FXC:
+    return point[F[V]] + C[O] ;
+  case FXF:
+    return point[F[V]] + point[F[O]] ;
+  case FCX:
+    return point[F[V]] * C[S] ;
+  case FCC:
+    return point[F[V]] * C[S] + C[O] ;
+  case FCF:
+    return point[F[V]] * C[S] + point[F[O]] ;
+  case FFX:
+    return point[F[V]] * point[F[S]] ;
+  case FFC:
+    return point[F[V]] * point[F[S]] + C[O] ;
+  case FFF:
+    return point[F[V]] * point[F[S]] + point[F[O]] ;
+    break;
+  case CXX:
+    return C[V];
+  case CXC:
+    return C[V] + C[O] ;
+    break;
+  case CXF:
+    return C[V] + point[F[O]] ;
+    break;
+  case CCX:
+    return C[V] * C[S] ;
+    break;
+  case CCC:
+    return C[V] * C[S] + C[O] ;
+    break;
+  case CCF:
+    return C[V] * C[S] + point[F[O]];
+    break;
+  case CFX:
+    return C[V] * point[F[S]] ;
+    break;
+  case CFC:
+    return C[V] * point[F[S]] + C[O] ;
+    break;
+  case CFF:
+    return C[V] * point[F[S]] + point[F[O]] ;
+    break;
+  }
+}
+
--- grace/src/Points.h
+++ grace/src/Points.h
@@ -13,17 +13,12 @@
 
 #include "juce.h"
 
-class PointField {
-  String name;
-  int type;
-  float dval;
-  double from;  // used for displaying in plotte
-  double to;
-  double by;
-  int ticks;
+// A PointField is basically a named index (offset) into a float*
+// point array. It also provides axis information used in plotting.
+// A PointDef defines its own collection of PointFields.
 
- public:
-  enum AxisType {
+class PointField {
+  enum FieldType {
     Float=0,
     Integer,
     Normalized,
@@ -32,12 +27,25 @@ class PointField {
     Amplitude,
     Seconds,
   };
+  String name;   // name of field
+  int index;     // value index into a float *point array
+  FieldType type; // what type of values field represents.
+  float dval;    // default value for point creation.
+  double from;   // lower value bound (used by plotter axis)
+  double to;     // upper value bound (plotter)
+  double by;     // tick increment along axis (plotter)
+  int ticks;     // number of sub ticks
+
+ public:
+
   PointField(XmlElement *xml) ;
   ~PointField() {}
   String getName() {return name;}
   void setName(String n) {name=n;}
-  int getType() {return type;}
-  void setType(int t) {type=t;}
+  FieldType getType() {return type;}
+  void setType(FieldType t) {type=t;}
+  int getIndex() {return index;}
+  void setIndex(int i) {index=i;}
   String getTypeName();
   float getDefault() {return dval;}
   void setDefault(float d) {dval=d;}
@@ -52,6 +60,133 @@ class PointField {
   int setTicks(int t) {ticks=t;}
   double getRange() {return to-from;}
   XmlElement* toXml();
+};
+
+class SoundMap;
+
+//
+// A PointDef holds infomation about user defined points. They are
+// imported/exported as xml text.
+//
+
+class PointDef {
+  String name;
+  OwnedArray<PointField> fields;
+  SoundMap *soundmap;
+ public:
+  PointDef(XmlElement *xml);
+  PointDef(PointDef &pd);  // deep copier
+  ~PointDef() {};
+
+  String getName() {return name; }
+  void setName(String n) {name=n; }
+  SoundMap *getSoundMap() {return soundmap;}
+  void *setSoundMap(SoundMap *s) {soundmap=s;}
+  PointField *getField(int i) { return fields[i]; }
+  PointField *findField(String n) {
+    for (int i=0; i<fields.size(); i++) {
+      PointField *f=getField(i);
+      if (f->getName() == n) 
+	return f;
+    }
+    return NULL;
+  }
+};
+
+
+//
+// Point->Sound Interface: SoundParameters and SoundMaps
+//
+
+// A SoundParameter transforms a user value (the Source) into a
+// value for a sound map: value=Source*Scaler+Offset. The Source
+// can be a point field (F) or a constant (C). Scaler and Offsets
+// are optional ie they are unused (X), Constant (C) or Field (F).
+
+class SoundParameter {
+
+  // Constant values of Source Scaler and Offset are kept in the 'C'
+  // array; field indexes are kept in the 'F' array. Each array is
+  // rank 3, where array[0] is the Source, array[1] is the Scaler, and
+  // array[2] is the Offset.
+
+  float C[3]; // C[0]=Source, C[1]=Scaler C[2]=Offset
+  int F[3];   // F[0]=Source, F[1]=Scaler F[2]=Offset
+
+  // The VSO enums are the indexes into the C and F arrays: index V
+  // references the Source, S the Scaler and O the Offset, ie
+  // C[V]=Source constant, C[S]=Scaler constant, C[O]=Offset constant;
+  // F[V]=Source field, F[S]=Scaler field, F[O]=Offset field
+
+  enum VSO {V=0, S, O}; 
+
+  // The CalcModes name all possible ways in which the value can be
+  // calculated from a Source Scaler and Offset. Each mode name
+  // consists of three letters: the first letter represents Source,
+  // the second the Scaler and the third the Offset. Letter meaning is
+  // C=constant, F=field, X=unused
+
+  enum CalcMode {
+    FXX=1, FXC, FXF, FCX, FCC, FCF, FFX, FFC, FFF,
+    CXX,   CXC, CXF, CCX, CCC, CCF, CFX, CFC, CFF,
+  };
+
+  // The specific CalcMode of the Soundparameter is determined when the
+  // SoundMap is parsed from its Xml entity.
+
+  CalcMode mode;
+
+public:
+
+  float getValue(float *point) ;
+};
+
+// A SoundMap is reponsible for transforming and writing point values
+// to some sound destination. Here is what a Table interface might
+// look like for a point called MIDI using a SoundMap called Midi:Note
+// _____________________________________________
+//                     Point: MIDI
+//              Source      Scaler     Offset
+// Midi:Note    --------------------------------
+//    time      [time ]
+//    dur       [duration]
+//    chan      [channel]
+//    key       [keynum]
+//    vel       [amplitude]  127.0
+// _____________________________________________
+
+class SoundMap {} ;
+
+class MidiMap : public SoundMap {
+  SoundParameter time;
+  SoundParameter chan;
+  //  virtual void writePoint(MidiMessageSequence seq, float *poi);
+  //  virtual void writePoint(MidiOutput port, float *poi);
+};
+
+class MidiNoteMap : public MidiMap {
+  SoundParameter dur;
+  SoundParameter key;
+  SoundParameter vel;
+};
+
+class MidiOnMap : public MidiMap {
+  SoundParameter key;
+  SoundParameter vel;
+};
+
+class MidiOffMap : public MidiMap {
+  SoundParameter key;
+  SoundParameter vel;
+};
+
+class MidiProgMap : public MidiMap {
+  SoundParameter prog;
+};
+
+class MidiCtrlMap : public MidiMap  {
+  SoundParameter ctrl;
+  SoundParameter val;
 };
 
 #endif
