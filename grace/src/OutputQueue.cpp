5dd95055878c00960e30a3abb75e2baec05e02c9
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2007-11-05 20:57:12 +0000

    fixed logic in run() and dont add nodes with time=0 to queue.
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@1445 d60aafaf-7936-0410-ba4d-d307febf7868

--- grace/src/OutputQueue.cpp
+++ grace/src/OutputQueue.cpp
@@ -21,13 +21,17 @@ OutputNode::OutputNode(double _time, float *vals, int num_vals)
 : time (0.0),num (0)
 { 
   int i = 0;
-  if(_time == 0.0) 
-    time = Time::getMillisecondCounterHiRes();
-  else
-    time = _time + Time::getMillisecondCounterHiRes();
+  //  if(_time == 0.0) 
+  //    time = Time::getMillisecondCounterHiRes();
+  //  else
+  time = _time + Time::getMillisecondCounterHiRes();
   
-  time = _time;
-  
+  //  time = _time;
+  //  if (vals[1] == 0) 
+  //    printf("adding note off %d at %d\n", (int) vals[0], (int) time);
+  //  else
+  //    printf("adding note On %d at %d\n", (int) vals[0], (int) time);
+ 
   for(i=0;i<num_vals;i++)
     values.add( vals[i]);
 }
@@ -37,12 +41,18 @@ OutputNode::~OutputNode()
 }
 
 void OutputNode::process() {
-  
-  if(values[1] == 0.0) 
-    outputQueue->output->sendMessageNow( MidiMessage::noteOff( (int)values[2], (int)values[0]) );
+  // array [0]=keynum, [1]=velocity, [2]=channel
+  if(values[1] == 0.0) {
+    outputQueue->output->sendMessageNow( MidiMessage::noteOff( (int)values[2],
+							       (int)values[0]) );
+    //    printf("sending off %d at %d\n", (int)values[0], 
+    //	   (int)Time::getMillisecondCounterHiRes());
+  }
   else 
   {
     outputQueue->output->sendMessageNow( MidiMessage::noteOn((int)values[2], (int)values[0], values[1] / 127.0f));
+    //    printf("sending On %d at %d\n", (int)values[0], 
+    //	   (int)Time::getMillisecondCounterHiRes());
     
   }
 }
@@ -64,27 +74,21 @@ void OutputQueue::run()
 {
   double curr;
   while( !threadShouldExit() ) {
-    
-    curr = Time::getMillisecondCounterHiRes() ;
-    
     while(outputNodes.size() > 0) {     
+      curr = Time::getMillisecondCounterHiRes() ;
       while ( outputNodes.getFirst()->time <= curr ) {       
-        
         outputNodes.lockArray();
-        
-        outputNodes.getFirst()->process();
-       
+	outputNodes.getFirst()->process();
         outputNodes.remove(0, true);
-        
         if(outputNodes.size() == 0) {
           outputNodes.unlockArray();
           break;
         }
         outputNodes.unlockArray();
-        curr = Time::getMillisecondCounterHiRes() ;
-
+	// im not sure that we want to do this, it
+        //curr = Time::getMillisecondCounterHiRes() ;
       }
-      usleep(100);
+      wait(1);
     }
     wait(-1);
   }
@@ -95,19 +99,24 @@ void OutputQueue::clear()
   outputNodes.lockArray();
   outputNodes.clear();
   outputNodes.unlockArray();
-  
-  
 }
 
 
 void OutputQueue::addNode(double _time, float* vals, int num_vals)
 {
   OutputNode *n = new OutputNode(_time, vals, num_vals);
-
   n->outputQueue = this;
-  outputNodes.lockArray();
-  outputNodes.addSorted(comparator, n);
-  outputNodes.unlockArray();	
-  notify();
+
+  // dont bother adding to queue if its immediate...
+  if ( _time == 0.0 ) {
+    n->process();
+    delete n;
+  }
+  else {
+    outputNodes.lockArray();
+    outputNodes.addSorted(comparator, n);
+    outputNodes.unlockArray();	
+    notify();
+  }
 }
 
