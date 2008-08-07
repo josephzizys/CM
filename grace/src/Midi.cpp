75ec6e2a72ed431a312db433b16ec542a5b89e8e
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2008-08-07 00:48:17 +0000

    test
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@1632 d60aafaf-7936-0410-ba4d-d307febf7868

--- grace/src/Midi.cpp
+++ grace/src/Midi.cpp
@@ -1,4 +1,5 @@
 /*************************************************************************
+ *
  * Copyright (C) 2007 Todd Ingalls, Rick Taube.                          *
  * This program is free software; you can redistribute it and/or modify  *
  * it under the terms of the Lisp Lesser Gnu Public License. The text of *
