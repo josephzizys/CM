82b0d395edb95009e2bdf4e1f872e3111bb90f4e
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2009-01-02 01:25:10 +0000

    premake script improved and cleaned up by carlos pita. tested on linux, windows and mac
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@1662 d60aafaf-7936-0410-ba4d-d307febf7868

--- premake.lua
+++ premake.lua
@@ -1,86 +1,40 @@
-addoption("juce", "Location of JUCE  directory")
-addoption("sndlib", "Optional location of SNDLIB directory")
-addoption("chicken", "Optional location of CHICKEN directory")
-
-juce = ""
-juce_debug = ""
-sndlib = ""
-chicken = ""
-chicken_include = ""
-chicken_lib = ""
-
-if not (options["clean"] or options["help"] or options["version"]) then
-   if not options["juce"] then
-      error("CM requires JUCE, add '--juce /path/to/juce' to your premake options.")
-   end
-   juce = options["juce"]
-   if not (string.sub(juce,string.len(juce)) == "/")  then
-      --insure slash at end of directory so concatenation works
-      juce = juce .. "/"
-   end
-   -- sanity check: dont build if juce.h is not found
-   if not os.fileexists(juce .. "juce.h") then
-      error("--juce directory does not contain juce.h")
-   end
-   -- arrg! juce debug lib can be either "libjuce_debug" or "jucedebug"
-   if os.fileexists(juce .. "bin/libjuce_debug.a") then
-      juce_debug="juce_debug"
-   elseif os.fileexists(juce .. "bin/libjucedebug.a") then
-      juce_debug="jucedebug"
-   else
-      print("WARNING: no JUCE debug lib found for CONFIG=Debug")
-   end
-   if options["sndlib"] then
-      sndlib = options["sndlib"]
-      -- insure slash at end directory so that concatenation works.
-      if not (string.sub(sndlib, string.len(sndlib)) == "/") then
-	 sndlib = sndlib .. "/"
-      end
-      -- sanity check: dont build if mus-config.h is not found
-      if not os.fileexists(sndlib .. "mus-config.h") then
-	 error("--sndlib directory does not contain mus-config.h")
-      end
-   elseif options["chicken"] then
-      chicken = options["chicken"]
-      --insure slash at end of directory so that concatenation works.
-      if not (string.sub(chicken, string.len(chicken)) == "/")  then
-	 chicken = chicken .. "/"
-      end
-      -- sanity check: dont attempt to build if chicken.h is not found
-      -- this is made more complex by that fact that on windows
-      -- chicken files are not in a single top-level folder
-      if os.fileexists(chicken .. "chicken.h") then
-	 chicken_include = chicken
-      elseif os.fileexists(chicken .. "include/chicken.h") then
-	 chicken_include = chicken .. "include/"
-      else
-	 error("--chicken directory does not contain chicken.h")	 
-      end
-
-      if os.fileexists(chicken .. "libchicken.a") then
-	 chicken_lib = chicken
-      elseif os.fileexists(chicken .. "lib/libchicken.a") then
-	 chicken_lib = chicken .. "lib/"
-      else
-	 error("--chicken directory does not contain libchicken.a")
-      end
-   else 
-      error("CM requires a Scheme implementation, add either '--sndlib /path/to/sndlib' or '--chicken /path/to/chicken' to your premake options.")
-   end
-end
-
-------------------------------------------
---           Global settings
-------------------------------------------
-
 project.name = "CM"
 project.bindir = "bin"
 project.configs = {"Release", "Debug"}
-mypackage = package -- premake sets 'package' to default package
+
+addoption("juce", "Location of JUCE source directory or install prefix")
+addoption("sndlib", "Optional location of SNDLIB source directory or install prefix")
+addoption("chicken", "Optional location of CHICKEN source directory or install prefix")
+
+sndlib = nil
+chicken = nil
+
+function insure_slash(path) 
+  --insure slash at end of directory so concatenation works
+  if not (string.sub(path, string.len(path)) == "/")  then
+     path = path .. "/"
+  end
+  return path
+end
+
+function add(t, element)
+   table.insert(t, element)
+end
+
+if not (options["clean"] or options["help"] or options["version"]) then
 
 for i = 1,2 do   
+   cm = i == 1
+   grace = i == 2
+
+------------------------------------------
+--           Global
+------------------------------------------
+
+   mypackage = newpackage() -- premake sets 'package' to default package
+   mypackage.includepaths = { "src" }
+   mypackage.linkflags = {"static-runtime"}
    mypackage.language = "c++"
-   -- the core set of files for all builds
    mypackage.files = {
       "src/Scanner.cpp",
       "src/Syntax.cpp",
@@ -92,117 +46,178 @@ for i = 1,2 do
       "src/Midi.cpp", 
       "src/Main.cpp"
    }
-   
-   if (i == 1) then
+   mypackage.config["Debug"].defines = {"DEBUG=1"}
+
+------------------------------------------
+--           CM
+------------------------------------------
+  
+   if (cm) then
       mypackage.name = "cm"
       mypackage.target = "cm"
       mypackage.kind = "exe"
       mypackage.objdir = "obj/cm"
-   else
+   end
+
+------------------------------------------
+--           Grace
+------------------------------------------
+
+   if (grace) then
       mypackage.name = "grace"
       mypackage.target = "Grace"
       mypackage.kind = "winexe"
       mypackage.objdir = "obj/grace"
-      mypackage.defines = {"GRACE=1"}
-      table.insert(mypackage.files, "src/Fonts.cpp")
-      table.insert(mypackage.files, "src/Help.cpp")
-      table.insert(mypackage.files, "src/Commands.cpp")
-      table.insert(mypackage.files, "src/Menus.cpp")
-      table.insert(mypackage.files, "src/TextEditor.cpp")
-      table.insert(mypackage.files, "src/Documentation.cpp")
-      table.insert(mypackage.files, "src/Images.cpp")
-      table.insert(mypackage.files, "src/Audio.cpp")
-   end   
-   
-   if options["juce"] then  -- not cleaning etc
-      -- scheme specific files
-      if options["chicken"] then
-	 table.insert(mypackage.files, "src/Chicken.cpp")
-	 table.insert(mypackage.files, "src/ChickenBridge.cpp")
-      elseif options["sndlib"] then
-	 table.insert(mypackage.files, "src/SndLib.cpp")
-	 table.insert(mypackage.files, "src/SndLibBridge.cpp")
-	 table.insert(mypackage.files, "src/Instruments.cpp")
-      end
-      
-      mypackage.includepaths = { juce, "src" }
-      mypackage.libpaths = {juce .. "bin"}
-      mypackage.linkflags = {"static-runtime"}
-      
-      if options["sndlib"] then
-	 table.insert(mypackage.defines, "SNDLIB=1")
-	 table.insert(mypackage.includepaths, sndlib)
-      elseif options["chicken"] then
-	 table.insert(mypackage.defines, "CHICKEN=1")
-	 table.insert(mypackage.includepaths, chicken_include)
-	 table.insert(mypackage.libpaths, chicken_lib)
-      end
-      
-      table.insert(mypackage.config["Debug"].defines, "DEBUG=1")
-      
-      ------------------------------------------
-      --           OS Specific 
-      ------------------------------------------
-      
-      mac_links = ""
-      linux_links = ""
-      
-      if macosx then
-	 table.insert(mypackage.defines, "MACOSX=1")
-	 mac_links =             " -framework Carbon "
-	 mac_links = mac_links .. "-framework Cocoa "
-	 mac_links = mac_links .. "-framework CoreServices "
-	 mac_links = mac_links .. "-framework CoreAudio "
-	 mac_links = mac_links .. "-framework CoreMidi "
-	 mac_links = mac_links .. "-framework ApplicationServices "
-	 mac_links = mac_links .. "-framework OpenGL "
-	 mac_links = mac_links .. "-framework AGL "
-	 mac_links = mac_links .. "-framework QuickTime "
-	 mac_links = mac_links .. "-framework IOKIT"
-	 
-	 if options["sndlib"] then
-	    mac_links = mac_links .. " " .. sndlib .. "sndlib.a"
-	 elseif options["chicken"] then
-	    mac_links = mac_links .. " " .. chicken .. "libchicken.a"
-	 end
-	 mypackage.config["Release"].links = {"juce" .. mac_links}
-	 mypackage.config["Debug"].links = {juce_debug .. mac_links}
-      elseif linux then
-	 table.insert(mypackage.defines, "LINUX=1")
-	 linux_links = " -l freetype"
-	 linux_links = linux_links .. " -l pthread"
-	 linux_links = linux_links .. " -l rt"
-	 linux_links = linux_links .. " -l X11"
-	 linux_links = linux_links .. " -l GL"
-	 linux_links = linux_links .. " -l GLU"
-	 linux_links = linux_links .. " -l Xinerama"
-	 linux_links = linux_links .. " -l asound"
-	 if options["sndlib"] then
-	    -- run the snd-config command to get the libs we need to
-	    -- include and add them to the linking set
-	    os.execute(sndlib .. "sndlib-config --libs > /tmp/sndlibs")
-	    for line in io.lines("/tmp/sndlibs") do 
-	       linux_links = linux_links .. " " .. line
-	    end
-	    --linux_links = linux_links .. " -l gsl"
-	    --linux_links = linux_links .. " -l gslcblas"
-	    --linux_links = linux_links .. " -l m"
-	    linux_links = linux_links .. " " .. sndlib .. "sndlib.a"
-	 elseif options["chicken"] then
-	    mac_links = mac_links .. " " .. chicken .. "libchicken.a"
-	 end
-	 table.insert(mypackage.libpaths, "/usr/X11R6/lib/")
-	 mypackage.config["Release"].links = {"juce" .. linux_links}
-	 mypackage.config["Debug"].links = {juce_debug .. linux_links}
-      elseif windows then
-	 table.insert(mypackage.defines, "WINDOWS=1")
-	 mingw_links = {"chicken", "juce", "gdi32", "comdlg32", "shell32", "ole32", "vfw32", "winmm", "wininet", "dsound", "wsock32", "opengl32", "glu32", "uuid", "rpcrt4" }
-	 mypackage.config["Release"].links = mingw_links
-      end
+      add(mypackage.defines, "GRACE=1")
+      add(mypackage.files, "src/Fonts.cpp")
+      add(mypackage.files, "src/Help.cpp")
+      add(mypackage.files, "src/Commands.cpp")
+      add(mypackage.files, "src/Menus.cpp")
+      add(mypackage.files, "src/TextEditor.cpp")
+      add(mypackage.files, "src/Documentation.cpp")
+      add(mypackage.files, "src/Images.cpp")
+      add(mypackage.files, "src/Audio.cpp")
    end
-   if (i == 1) then
-      mypackage = newpackage()
+
+------------------------------------------
+--           Juce
+------------------------------------------
+
+   if options["juce"] then
+      juce = insure_slash(options["juce"])
+      juce_lib = "juce"
+      if os.fileexists(juce .. "juce.h") then
+         add(mypackage.includepaths, juce)
+         add(mypackage.libpaths, juce .. "bin")
+         if os.fileexists(juce .. "bin/libjuce_debug.a") then
+            juce_debug_lib = "juce_debug"
+         elseif os.fileexists(juce .. "bin/libjucedebug.a") then
+            juce_debug_lib = "jucedebug"
+         else
+            juce_debug_lib = "juce"
+            print("WARNING: no juce debug lib found for CONFIG=Debug")
+         end
+      elseif os.fileexists(juce .. "include/juce/juce.h") then
+         add(mypackage.includepaths, juce .. "include/juce")
+         add(mypackage.libpaths, juce .. "lib")
+         juce_debug_lib = "juce"
+         print("WARNING: no juce debug lib when linking against installed juce")
+      else
+         error("--juce must point to either juce source directory or juce install prefix (eg. /usr or /usr/local)")
+      end
+      mypackage.config["Release"].links = { juce_lib }
+      mypackage.config["Debug"].links = { juce_debug_lib }
+   else
+      error("CM requires juce, add --juce to your premake options")
+   end
+   
+------------------------------------------
+--           Sndlib
+------------------------------------------
+   
+   if options["sndlib"] then
+      sndlib = insure_slash(options["sndlib"])
+      add(mypackage.defines, "SNDLIB=1")
+      add(mypackage.files, "src/SndLib.cpp")
+      add(mypackage.files, "src/SndLibBridge.cpp")
+      add(mypackage.files, "src/Instruments.cpp")
+      if os.fileexists(sndlib .. "mus-config.h") then
+         add(mypackage.includepaths, sndlib)
+         add(mypackage.linkoptions, sndlib .. "sndlib.a")
+         sndlib_config = sndlib .. "sndlib-config"
+      elseif os.fileexists(sndlib .. "include/mus-config.h") then
+         add(mypackage.includepaths, sndlib .. "include")
+         add(mypackage.libpaths, sndlin .. "lib")
+         add(mypackage.links, "sndlib")
+         sndlib_config = sndlib .. "bin/sndlib-config"
+      else
+	 error("--sndlib must point to either sndlib source directory or sndlib install prefix (eg. /usr or /usr/local)")
+      end
+      if linux then
+         -- run the snd-config command to get the libs we need to
+         -- include and add them to the linking set
+         os.execute(sndlib_config .. " --libs > /tmp/sndlibs")
+         for line in io.lines("/tmp/sndlibs") do 
+            add(mypackage.linkoptions, line)
+         end
+      end
+
+------------------------------------------
+--           Chicken
+------------------------------------------
+   
+   elseif options["chicken"] then
+      chicken = insure_slash(options["chicken"])
+      add(mypackage.defines, "CHICKEN=1")
+      add(mypackage.files, "src/Chicken.cpp")
+      add(mypackage.files, "src/ChickenBridge.cpp")
+      if os.fileexists(chicken .. "chicken.h") then
+         add(mypackage.includepaths, chicken)
+         add(mypackage.linkoptions, chicken .. "libchicken.a")
+      elseif os.fileexists(chicken .. "include/chicken.h") then
+         add(mypackage.includepaths, chicken .. "include")
+         add(mypackage.libpaths, chicken .. "lib")
+         add(mypackage.links, "chicken")
+      else
+         error("--chicken must point to either chicken source directory or chicken install prefix (eg. /usr or /usr/local)")
+      end
+   else
+      error("CM requires a Scheme implementation, add either --sndlib or --chicken to your premake options")
+   end
+   
+------------------------------------------
+--           OS Specific 
+------------------------------------------
+         
+   if macosx then
+      add(mypackage.defines, "MACOSX=1")
+      add(mypackage.linkoptions, "-framework Carbon")
+      add(mypackage.linkoptions, "-framework Cocoa")
+      add(mypackage.linkoptions, "-framework CoreServices")
+      add(mypackage.linkoptions, "-framework CoreAudio")
+      add(mypackage.linkoptions, "-framework CoreMidi")
+      add(mypackage.linkoptions, "-framework ApplicationServices")
+      add(mypackage.linkoptions, "-framework OpenGL")
+      add(mypackage.linkoptions, "-framework AGL")
+      add(mypackage.linkoptions, "-framework QuickTime")
+      add(mypackage.linkoptions, "-framework IOKIT")
+   elseif linux then
+      add(mypackage.defines, "LINUX=1")
+      add(mypackage.links, "pthread")
+      add(mypackage.links, "rt")
+      add(mypackage.links, "X11")
+      add(mypackage.links, "GL")
+      add(mypackage.links, "GLU")
+      add(mypackage.links, "Xinerama")
+      add(mypackage.links, "asound")
+      add(mypackage.links, "freetype")
+      add(mypackage.libpaths, "/usr/X11R6/lib/")
+   elseif windows then
+   
+      -- premake outputs libs defined at the config level BEFORE slibs
+      -- defined at the package level. on windows this causes linking
+      -- to fail because -ljuce appears after the libs listed below (i
+      -- dont know why this should be the case but it is). this means
+      -- that to ensure -ljuce appears BEFORE these libs we add them
+      -- to the configs rather than to the package as on linux an
+      -- macos. 
+      for c in {"Release", "Debug"} do
+         add(mypackage.defines, "WINDOWS=1")
+         add(mypackage.config[c].links, "gdi32")
+         add(mypackage.config[c].links, "comdlg32")
+         add(mypackage.config[c].links, "shell32")
+         add(mypackage.config[c].links, "ole32")
+         add(mypackage.config[c].links, "vfw32")
+         add(mypackage.config[c].links, "winmm")
+         add(mypackage.config[c].links, "wininet")
+         add(mypackage.config[c].links, "dsound")
+         add(mypackage.config[c].links, "wsock32")
+         add(mypackage.config[c].links, "opengl32")
+         add(mypackage.config[c].links, "glu32")
+         add(mypackage.config[c].links, "uuid")
+         add(mypackage.config[c].links, "rpcrt4")
+      end
    end
 end
 
--- eof
+end
