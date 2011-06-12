afb634e531ae653960d8932622cd7b3247233de8
taube <taube@d60aafaf-7936-0410-ba4d-d307febf7868>
2011-06-12 13:28:54 +0000

    new plotting examples
    git-svn-id: https://commonmusic.svn.sourceforge.net/svnroot/commonmusic/trunk@1950 d60aafaf-7936-0410-ba4d-d307febf7868

--- src/Resources.h
+++ src/Resources.h
@@ -9,7 +9,7 @@ namespace Resources
     const int           doc_xmlSize = 3287;
 
     extern const char*  doc_zip;
-    const int           doc_zipSize = 159364;
+    const int           doc_zipSize = 159697;
 
 };
 
